43dec93b1adc19fd77caac3fdba83d6b92f64e2f
chirag1749 <42379666+chirag1749@users.noreply.github.com>
2018-08-14 22:25:41 -0400

    mySiteOne

---
+++ MySiteOne/App/app.config.js
@@ -0,0 +1,15 @@
+﻿(function () {
+  'use strict';
+  angular.module('mySiteOne').config(appConfig);
+
+  appConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function appConfig($stateProvider, $urlRouterProvider) {
+
+    $stateProvider.state('home', {
+      url: '',
+      templateUrl: 'app/mySiteOne.html'
+    });
+  }
+
+})();
---
+++ MySiteOne/App/mySiteOneController.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+    angular.module('mySiteOne').controller('mySiteOneController', mySiteOneController);
+
+    function mySiteOneController() {
+        var vm = this;
+
+        Init();
+
+        function Init() {
+            vm.message = "Hello World";
+        }
+    }
+}());
